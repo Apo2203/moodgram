e7ce8a0a549d51f3c0a291e9d2e2009126f37d53
Francesco Apollonio <apollonio_francesco@libero.it>
2022-11-29 10:36:43 +0200

    Signup
    system is still not able to manage a situation where someone try to register an account with an email already registered

---
+++ dataBase/processSignUp.php
@@ -0,0 +1,50 @@
+<?php
+if (empty($_POST["name"])) {
+    die ("Name is required");
+}
+if (! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die ("Email is not valid");
+}
+if (strlen($_POST["password"]) < 8) {
+    die("Password must be at least 8 characters");
+}
+if ( ! preg_match("/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/", $_POST["password"])){
+    die("Your Password should contain at least 8 characters, 1 capital letter and 1 number");
+}
+if ($_POST["password"] !== $_POST["passwordConfirm"]){
+    die("The two passwords must be the same");
+}
+$hashedPassword = password_hash($_POST["password"], PASSWORD_DEFAULT);
+$mysqli = require __DIR__ . "/database.php";
+
+
+$select = mysqli_query($mysqli, "SELECT * FROM users WHERE email = '".$_POST["email"]."'");
+if(mysqli_num_rows($select)) {
+    exit('This email address is already used!');
+}
+if (! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$sql = "INSERT INTO user (name, surname, email, password_hash, age)
+        VALUES (?, ?, ?, ?, ?)";
+
+
+$stmt = $mysqli->stmt_init();
+
+
+
+$stmt->bind_param("ssssi",
+                  $_POST["name"],
+                  $_POST["surname"],
+                  $_POST["email"],
+                  $hashedPassword,
+                  $_POST["age"]);
+                  
+if ($stmt->execute()) {
+    echo "Signup completed succesfully!";
+} 
+else {
+    die($mysqli->error . " " . $mysqli->errno);
+}
+?>

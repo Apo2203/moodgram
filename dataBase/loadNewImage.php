c2158c0a965edbb754b5880a5b8fb621d9a2470e
Francesco Apollonio <apollonio_francesco@libero.it>
2023-01-02 12:28:09 +0100

    profile picture change allowed

--- dataBase/loadNewImage.php
+++ dataBase/loadNewImage.php
@@ -1,7 +1,9 @@
 <?php
+$mysqli = require __DIR__ . "/database.php";
+session_start();
 
 if (isset($_POST["submit"]) && isset($_FILES["newImage"])){
-    
+
     $img_name = $_FILES["newImage"]["name"];
     $img_size = $_FILES["newImage"]["size"];
     $tmp_name = $_FILES["newImage"]["tmp_name"];
@@ -9,8 +11,7 @@ if (isset($_POST["submit"]) && isset($_FILES["newImage"])){
 
     if ($error == 0){
         if($img_size > 225000){
-            $em = "Sorry, your file is too big";
-            header("Location: ../WebPages/setting.php?error=$em");
+            echo ("Sorry, your file is too big");
         }
         else{
             //image extension
@@ -20,19 +21,31 @@ if (isset($_POST["submit"]) && isset($_FILES["newImage"])){
 
             $allowed_exs = array ("jpg", "jpeg", "png");
             if(in_array($img_ex_lc, $allowed_exs)){
+                // Saving the new image on the server
                 $new_img_name = uniqid("IMG-", true).'.'.$img_ex_lc;
                 $img_upload_path = "../assets/img/profilePictureImage/".$new_img_name;
                 move_uploaded_file($tmp_name, $img_upload_path);
+
+                // Insert into Database
+                $sql = "UPDATE user SET profilePicture = ? WHERE id = ?";
+                $stmt = $mysqli->stmt_init();
+                if (! $stmt->prepare($sql)) {
+                die("SQL error: " . $mysqli->error);
+                }
+                $stmt->bind_param("si",
+                    $new_img_name,
+                    $_SESSION["user_id"]
+                );
+                $stmt->execute();
+                header("Location: ../WebPages/setting.php");
             }
             else{
-                $em = "Type of file not allowed";
-                header("Location: ../WebPages/setting.php?error=$em");
+                echo "Type of file not allowed";
             }
         }
     }
     else{
-        $em = "unknown error occurred!";
-        header("Location: ../WebPages/setting.php?error=$em");
+        echo "unknown error occurred!";
     }
 
 }
